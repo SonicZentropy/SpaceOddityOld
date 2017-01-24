bc917385eea4821c1a35e46caffb9ae1404d5155
Dylan Bailey <SonicZentropy@gmail.com>
2017-01-24 05:26:55 -0600

    Popcorn warfare added

---
+++ Assets/Standard Assets/PopcornFX/WarfareHDPack_Demo/_Sources/Scripts/WFP_AmmoTrailBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WFP_AmmoTrailBehaviour : MonoBehaviour {
+
+	public float force = 1000;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().AddForce(transform.right * force);
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+	}
+}
